c46df38c674a99d8f512f5ec8a4ce77d3d1c40fb
lmarcarini <leonardo.marcarini@yahoo.com.br>
2022-03-21 14:44:57 -0300

    Changed to typescript

---
+++ layout/layout.tsx
@@ -0,0 +1,10 @@
+export default function Layout({children}: React.PropsWithChildren<{}>) {
+  return (
+      <>
+      <div>Create Top Menu</div>
+    <div>
+        {children}
+    </div>
+    </>
+  )
+}
